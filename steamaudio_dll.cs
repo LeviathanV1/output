e3290f9f94b55ec9d6577c0e0d0b7cc82e5f5ebd
ClopsGamerV2 <265656011+LeviathanV1@users.noreply.github.com>
2026-05-02 16:26:12 -0300

    123

--- steamaudio_dll.cs
+++ steamaudio_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-29 20:53:53.196343500 UTC
+// 2026-05-02 19:23:02.725165600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: steamaudio.dll
